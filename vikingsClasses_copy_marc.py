f9f78395ef05eb7827451ecbc6294e6d5cf2e63a
Marc Jahnert <marc.jahnert@gmail.com>
2025-02-20 13:25:50 +0100

    copies_marc

--- vikingsClasses_copy_marc.py
+++ vikingsClasses_copy_marc.py
@@ -2,38 +2,48 @@ import random
 
 # Soldier
 
-
 class Soldier:
     def __init__(self, health, strength):
-        # your code here
-    
+        self.health = health
+        self.strength = strength
+
     def attack(self):
-        # your code here
+        return self.strength
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+
     
 
 # Viking
 
 class Viking(Soldier):
     def __init__(self, name, health, strength):
-        # your code here
-
-    def battleCry(self):
-        # your code here
+        super().__init__(health, strength)
+        self.name = name
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
 class Saxon(Soldier):
     def __init__(self, health, strength):
-        # your code here
+        super().__init__(health, strength)
 
     def receiveDamage(self, damage):
-        # your code here
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return f"A Saxon has died in combat"
 
 # Davicente
 
